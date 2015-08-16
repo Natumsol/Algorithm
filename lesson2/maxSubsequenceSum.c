7d1173452923f789e7121fcede35a5fe3b02b7a9
natumsol <ly900208>
2015-08-17 00:41:45 +0800

    add algorithm 4

--- lesson2/maxSubsequenceSum.c
+++ lesson2/maxSubsequenceSum.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 int main() {
-	int array[3000], i, temp;
+	int array[30000], i, temp;
 	clock_t  t_start, t_end;
 	srand((unsigned)time(NULL));//用当前时间，设置种子 
-	for (i = 0; i < 3000; i ++) {
+	for (i = 0; i < 30000; i ++) {
 		temp = rand()%1000;
 		if (temp % 2) temp = -temp;
 		array[i] = temp;
 	}
-	t_start = clock() ;
+	/*t_start = clock() ;
 	temp = maxSubsequenceSum1(array, 3000);
 	t_end = clock() ;
 	printf("最大序列为:%d, 执行时间为:%d ms\n",temp, t_end - t_start);
@@ -21,9 +21,15 @@ int main() {
 	printf("最大序列为:%d, 执行时间为:%d ms\n",temp, t_end - t_start);
 
 	t_start = clock() ;
-	temp = maxSubsequenceSum3(array, 3000);
+	temp = maxSubsequenceSum3(array, 300000);
+	t_end = clock() ;
+	printf("最大序列为:%d, 执行时间为:%d ms\n",temp, t_end - t_start);*/
+
+	t_start = clock() ;
+	temp = maxSubsequenceSum4(array, 30000);
 	t_end = clock() ;
 	printf("最大序列为:%d, 执行时间为:%d ms\n",temp, t_end - t_start);
+
 	getchar();
 	return 0;
 }
@@ -95,4 +101,16 @@ int maxSubsequenceSum3(int A[], int N) {
 int Max3(int a, int b, int c){
 	int temp = a > b ? a :b;
 	return temp > c ? temp : c;
+}
+
+int maxSubsequenceSum4(int A[], int N){
+	int thisMax = 0, maxSum = 0, i;
+	for(i = 0; i < N; i ++) {
+		thisMax += A[i];
+		if(thisMax > maxSum)
+			maxSum = thisMax;
+		else if(thisMax < 0)
+			thisMax = 0;
+	}
+	return maxSum;
 }
